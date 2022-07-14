3746230c3982f7ae63ae9ea6d0b0f5206bf177ba
Khoshanlo Sharip <khoshanlosharip@gmail.com>
2022-07-15 01:10:43 +0600

    mediaqueries and js added

---
+++ orders.js
@@ -0,0 +1,33 @@
+const Orders = [
+    {
+        productName: 'Fordable Mini Drone',
+        productNumber: '631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'football kit',
+        productNumber: '243',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Basketball skirt',
+        productNumber: '543',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Mac laptop',
+        productNumber: '446',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Gps Navigator',
+        productNumber: '125',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    
+]
